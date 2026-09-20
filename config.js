7aa82fa5bdcb879b9e61c07e0d92c2510b31b749
JoKErRKS <jokerxlegend3@gmail.com>
2026-09-20 14:11:06 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: '8978182490:AAGV6-WgTAZY3f5mccAglRuGrzhK5RyUL50', // Bot Token Here 
+    
+    S7: 'Mithu_MD BUG', // Owner, Name
+    
+    adminId: '7322038106', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/teammysterybyali', // TG channel link
+    
+    group: 'https://t.me/banproofsbyali', // TG group link
+    
+    bot: 'Mithu_MD BUG', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
